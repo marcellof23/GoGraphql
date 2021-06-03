832bd50b7db3ebbc70e05dc803bb26b56a3a171c
Marcello Faria <01faria.marcello@gmail.com>
2021-06-03 11:16:26 +0700

    finishing all unit testing

--- internal/handlers/ktp_test.go
+++ internal/handlers/ktp_test.go
@@ -7,40 +7,45 @@ import (
 	"github.com/machinebox/graphql"
 )
 
-// func TestCreateUser(t *testing.T) {
-// 	var client = graphql.NewClient("http://localhost:8080/query")
+func TestCreateUser(t *testing.T) {
+	var client = graphql.NewClient("http://localhost:8080/query")
 
-// 	var req = graphql.NewRequest(`
-// 			mutation {
-// 				createUser(input : {
-// 					nik:           "135",
-// 					nama:          "ronda",
-// 					alamat:        "taman anggrek",
-// 					jenisKelamin:  "male",
-// 					tanggalLahir:  "2011-01-02 15:04:05",
-// 					agama:         "buddha"
-// 				}){
-// 					nama
-// 				}
-// 			}
-// 		`)
+	var req = graphql.NewRequest(`
+			mutation {
+				createUser(input : {
+					nik:           "135",
+					nama:          "ronda",
+					alamat:        "taman anggrek",
+					jenisKelamin:  "male",
+					tanggalLahir:  "2011-01-02 15:04:05",
+					agama:         "buddha"
+				}){
+					nik
+				nama
+				alamat
+				jenisKelamin
+				tanggalLahir
+				agama
+				}
+			}
+		`)
 
-// 	ctx := context.Background()
+	ctx := context.Background()
 
-// 	var respData map[string]interface{}
-// 	if err := client.Run(ctx, req, &respData); err != nil {
-// 		t.Error(err)
-// 	}
+	var respData map[string]interface{}
+	if err := client.Run(ctx, req, &respData); err != nil {
+		t.Error(err)
+	}
 
-// }
+}
 
-func UpdateUser(t *testing.T) {
+func TestUpdateUser(t *testing.T) {
 	var client = graphql.NewClient("http://localhost:8080/query")
 
 	var req = graphql.NewRequest(`
 		mutation {
 			updateUser(id : "2", input : {
-				nik:           "1354",
+				nik:           "13545",
 				nama:          "asdfronda",
 				alamat:        "taman anggrek",
 				jenisKelamin:  "male",
@@ -65,21 +70,19 @@ func UpdateUser(t *testing.T) {
 	}
 }
 
-// func DeleteUser(t *testing.T) {
-// 	var client = graphql.NewClient("http://localhost:8080/query")
+func TestDeleteUser(t *testing.T) {
+	var client = graphql.NewClient("http://localhost:8080/query")
 
-// 	var req = graphql.NewRequest(`
-// 		mutation deleteUser {
-// 			deleteUser(id : "2")
-// 		} {
-// 			nama
-// 		}
-// 	`)
+	var req = graphql.NewRequest(`
+		mutation deleteUser {
+			deleteUser(id : "1")
+		}
+	`)
 
-// 	ctx := context.Background()
+	ctx := context.Background()
 
-// 	var respData map[string]interface{}
-// 	if err := client.Run(ctx, req, &respData); err != nil {
-// 		t.Error(err)
-// 	}
-// }
+	var respData map[string]interface{}
+	if err := client.Run(ctx, req, &respData); err != nil {
+		t.Error(err)
+	}
+}
